3e485447cb84a5d661c718c6d3b5bf266afbcc01
Dilip kumar singh <117737382+sdillipkumar@users.noreply.github.com>
2025-06-01 15:39:57 +0530

    Update settings.py

--- office_emp_proj/settings.py
+++ office_emp_proj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,11 @@ SECRET_KEY = 'django-insecure-*2mr@=j@bhpoxi#zc-iw-bkg&l28y(!ztmox&0(tm7o78+shtv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ALLOWED_HOSTS = [
+    'office-employees-management.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]]
 
 
 # Application definition
@@ -77,10 +82,11 @@ WSGI_APPLICATION = 'office_emp_proj.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback (optional)
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
