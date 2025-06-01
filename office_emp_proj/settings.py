4406f7fbd5f1654c275fddc84622c5306916b5f1
Dilip kumar singh <117737382+sdillipkumar@users.noreply.github.com>
2025-06-01 20:01:33 +0530

    Update settings.py

--- office_emp_proj/settings.py
+++ office_emp_proj/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-*2mr@=j@bhpoxi#zc-iw-bkg&l28y(!ztmox&0(tm7o78+shtv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ['office-employees-management.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
